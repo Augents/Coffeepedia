ef6d103e2283d06fc7de119e132b29aae30ca784
Thakshin N <thakshinnaidoo@gmail.com>
2023-08-16 22:15:10 +0200

    Moved Supabasekey and SupabaseUrl to .env

---
+++ coffee_app/src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_KEY, SUPABASE_URL } from '$env/static/private'
+
+export const supabaseClient = createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY,
+)
