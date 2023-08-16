660bc5b008b7167005ef925eba5118933b506eaa
GyuoMi <darshnsingh10@gmail.com>
2023-08-16 20:16:01 +0200

    added env secret to workflow yml

--- coffee_app/src/routes/api/database-api.ts
+++ coffee_app/src/routes/api/database-api.ts
@@ -1,4 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 import config  from './database-config.js'
-const supabaseKey =  ${{secrets.SUPABASE_SERVICE_ROLE_KEY}} 
-const supabase = createClient(config.supabaseUrl, supabaseKey)
+const supabase = createClient(config.supabaseUrl, config.supabaseKey)
